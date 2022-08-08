1225a4d76037bfc44d75218a9391810ccc8972c1
linx <linx9581@gmail.com>
2022-08-08 10:04:27 +0800

    opt

---
+++ js/array/repeatStr.js
@@ -0,0 +1,8 @@
+let nn = ['裴洛西','七夕','台股']
+let google = ['飛彈','七夕情人節快樂','2022七夕']
+
+console.log(google.includes('七夕'));
+console.log(google.includes('2022七夕'));
+
+console.log(nn[0][0]);
+console.log(nn[0].length);
